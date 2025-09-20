62b0e44ac40d532340fdc6d23971bd8fab037abc
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-09-20 12:29:48 +0000

    studied about comparisons check

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,6 +6,7 @@ console.log(typeof (score));
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
 
 //"33"=>33
 // "33abc"=>NaN
@@ -19,4 +20,38 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 let stringConversion = String(someNumber)
 console.log(stringConversion);
-console.log(typeof stringConversion);
+console.log(typeof stringConversion); */
+
+//////////////////////////////////////////OPERATIONS///////////////////////////////////////////////////////
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Ayush"
+let str3 = str1 + str2 
+//console.log(str3)
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2   //not recommended
+//console.table([num1,num2,num3])
+
+let counter = 100
+++counter
+//console.log(counter)
+//Typescript doesn't allow to compare or use two datatypes together but js does
+//ALWAYS AVOID THESE TYPES OF CONVERSIONS
+//console.log(2+"3")   //unpredictable behaviour
+//console.log("2">1)  //not recommended
+
+/*console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)     //comparison and equality checks work differently in javascript
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)*/
+
+console.log("2"===2)   //js === ke case me string ko convert nhi kr paata h no me but == me kr leta h
+
+
