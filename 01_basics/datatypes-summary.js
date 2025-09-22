150484247dd2555e3feebfb6114b590a72be2776
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-09-22 10:28:14 +0000

    ok

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+/*In Js datatypes are categorised into two types based on how the data is stored and accessed
+
+# Primitive(Call by value): esme jab bhi data copy hota hai, memory reference nhi diya jaata hai, balki iska copy diya jata hai and usme hi changes kiye jaate hain!!
+
+7 types: String , Number , Null , Undefined , boolean , symbol , BigInt
+
+# Non-primitive(Call by reference) 
+
+3 types : arrays , Functions , Objects*/
