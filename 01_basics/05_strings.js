0905cdde927cb5eba8331c768efff0cdf6851755
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-09-23 18:33:42 +0000

    object & commands

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,7 +2,7 @@
 const repoCount = 50
 
 console.log ( name + repoCount + " Value " )
-console.log ( `${name} ${repoCount}`)*/
+console.log ( `${name} ${repoCount}`)
 
 const gameName = new String ('Yush-ku') //String object
 console.log ( gameName )   
@@ -14,8 +14,21 @@ console.log (gameName.toUpperCase())
 console.log (gameName.length)
 console.log (gameName.substring(0,4))  //doesn't obey minus
 console.log (gameName.slice(-6,4))     // obeys minus
+*/
+const newStringOne = ' Aayush   '
+console.log (newStringOne)
+console.log (newStringOne.trim())
 
 
+const url = "https://Ayush.com/amisha%20jha"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('Ayush'))
+console.log(url.includes('Rahul'))
+
+const strArr = new String ('Ayush-is-very-nice')
+
+console.log(strArr.split('-'))
 
 
 
