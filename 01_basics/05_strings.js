306daca42c71db48f2cb4e66596945ee76a8d299
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-09-23 16:48:52 +0000

    objects & functions

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+/*const name = "Ayush"                       // String
+const repoCount = 50
+
+console.log ( name + repoCount + " Value " )
+console.log ( `${name} ${repoCount}`)*/
+
+const gameName = new String ('Yush-ku') //String object
+console.log ( gameName )   
+console.log (gameName[0]);
+console.log (gameName.__proto__)
+console.log (gameName.charAt(5))
+console.log (gameName.indexOf('h'))
+console.log (gameName.toUpperCase())
+console.log (gameName.length)
+console.log (gameName.substring(0,4))  //doesn't obey minus
+console.log (gameName.slice(-6,4))     // obeys minus
+
+
+
+
+
+
+
