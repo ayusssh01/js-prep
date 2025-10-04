2b7522079575ae4c5caf4fff61f00ab08df01c92
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-10-04 15:18:14 +0000

    removed comments from array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,7 +4,7 @@
 (4) A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you can be assured you're not causing the other object to change too. That behavior contrasts with the behavior of a shallow copy, in which changes to nested properties in the source or the copy may cause the other object to change too.*/
 
 const myArr = [1,2,3,4,5];
-//console.log(myArr[0]);
+console.log(myArr[0]);
 
 const myArr2 = new Array(1,2,3,4);  //this style of constructor array or Array object faces problem when there is a single element, it takes that single element as the size of the array!
 //console.log(myArr2[0]);
@@ -12,7 +12,7 @@ const myArr2 = new Array(1,2,3,4);  //this style of constructor array or Array o
 
 // ARRAY METHODS
 
-/*myArr.push(6)   //inserts 6 at the back
+myArr.push(6)   //inserts 6 at the back
 myArr.push(7)   //inserts 7 at the back
 myArr.pop();    //takes out 7(i.e, last inserted element)
 
@@ -24,7 +24,7 @@ console.log(myArr.indexOf(9))  //gives -1 for element which doesn't exist
 console.log(myArr2.indexOf(4))
 const newArr = myArr.join()  //converts the array into string
 console.log(newArr);
-console.log(typeof newArr);  // gives string*/
+console.log(typeof newArr);  // gives string
 
 // slice , splice
 
