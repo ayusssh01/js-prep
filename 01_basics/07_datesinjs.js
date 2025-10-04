d88a1cd970f502d7cca3b7e46116513d895db3b3
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-10-04 13:59:28 +0000

    removed comments

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,5 +1,5 @@
 let myDate = new Date();
-/*console.log(myDate);
+console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toISOString());
 console.log(myDate.toDateString());
@@ -7,18 +7,18 @@ console.log(myDate.toLocaleString());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleTimeString());
 console.log(typeof myDate);   ///object
-let myCreatedDate = new Date(2025, 0, 25);   //month starts with 0 in js
+/*let myCreatedDate = new Date(2025, 0, 25);   //month starts with 0 in js
 console.log(myCreatedDate.toDateString());  //Sat Jan 25 2025
 let myCreatedDate = new Date(2025, 0, 25 , 5 , 3);
 console.log(myCreatedDate.toLocaleString()); // print --> 1/25/2025, 5:03:00 AM
-let myCreatedDate = new Date("2025-01-25");
+let myCreatedDate = new Date("2025-01-25");*/
 //console.log(myCreatedDate.toLocaleString());   // print --> 1/25/2025, 12:00:00 AM
 let myTimeStamp = Date.now();
 console.log(myTimeStamp);   //gives current time in milliseconds
 console.log(myCreatedDate.getTime());   // gives milliseconds fixed of the particular date
 //console.log(Math.floor(myTimeStamp/1000)); // converts ms into sec
 let diff = myTimeStamp - myCreatedDate.getTime();
-console.log(Math.floor(diff/(1000*60*60*24)));/// tells how many days've passed since 25th Jan'25*/
+console.log(Math.floor(diff/(1000*60*60*24)));/// tells how many days've passed since 25th Jan'25
 let newDate =  new Date()
 console.log(newDate);
 console.log(newDate.getMonth()+1);
