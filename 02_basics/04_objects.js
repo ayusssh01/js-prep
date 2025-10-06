f5c1f6798e2c9b7c83067bd24e81d015a09725b2
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-10-06 20:39:46 +0000

    objects part2 over

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,12 +30,75 @@ const obj2 = {3:"c",4:"d"}
 const obj3 = {...obj1,...obj2}  // most preferred,spread
 console.log(obj3)
 
-const user = [{
+const user = [{         /// database se aisa hi milta h
     id : "149",
     email : "Aryan1708@gmail.com"},
     {
     id : "150",
     email : "Aryan1709@gmail.com"
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
     }
+    
 ]
 console.log(user[1].email)
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser)) //[ 'ID', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)) //[ '123abc', 'Ayush', false ]
+console.log(Object.entries(tinderUser)) // [ [ 'ID', '123abc' ], [ 'name', 'Ayush' ], [ 'isLoggedIn', false ] ]
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))  //true
+console.log(tinderUser.hasOwnProperty("isLogged")) //false  
+
+/*[[Prototype]]
+: 
+Object
+constructor
+: 
+ƒ Object()
+hasOwnProperty
+: 
+ƒ hasOwnProperty()
+isPrototypeOf
+: 
+ƒ isPrototypeOf()
+propertyIsEnumerable
+: 
+ƒ propertyIsEnumerable()
+toLocaleString
+: 
+ƒ toLocaleString()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf()
+__defineGetter__
+: 
+ƒ __defineGetter__()
+__defineSetter__
+: 
+ƒ __defineSetter__()
+__lookupGetter__
+: 
+ƒ __lookupGetter__()
+__lookupSetter__
+: 
+ƒ __lookupSetter__()
+__proto__
+: 
+(...)
+get __proto__
+: 
+ƒ __proto__()
+set __proto__
+: 
+ƒ __proto__()*/
