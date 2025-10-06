b09516fb1b3cf0203a3b07721ab3e2be4b651cca
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-10-06 19:23:31 +0000

    objects part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,14 +1,16 @@
 //Singleton --> jab constructor se object bnate h toh ye bnta h not when we declare it like literals
 //object literals
+// symbol ko object me keys ke roop me add kiya jaata hai, kind of weak encapsulation maana jaata h
 
-const mySim1 = Symbol("key1");  //"key1" is just a description, not the key name in your object.
+
+const mySym = Symbol("key1");  //"key1" is just a description, not the key name in your object.
 //It’s only used to make it easier to identify the Symbol when you log it.So "key1" = label/tag for the Symbol, not the property value. "myKey1" — the value stored in the object
 
 const jsUser = {   // we can define it with both keys & values and access the object through both too
     name : "Ayush", // But in array , we only define the value.
     "full name" : "Ayush Jha",
-     //mySim1 : "myKey1", //gives correct output but wrong, the type is string
-    [mySim1] : "myKey1", //correct way, the type is symbol //Use the Symbol stored in mySim1(key 1) as a property key,and assign it the value 'myKey1'.”
+     //mySym : "myKey1", //gives correct output but wrong, the type is string
+    [mySym] : "myKey1", //correct way, the type is symbol //Use the Symbol stored in mySym(key1)                               as a property key,and assign it the value 'myKey1'.”
     age : 22,
     location : "Kolkata",
     email : "Ayush2022@iem.edu.in",
@@ -20,15 +22,28 @@ console.log(jsUser.email)   // generally people use but doesn't work always
 console.log(jsUser["email"]) 
 //console.log(jsUser.full name) // error here
 console.log(jsUser["full name"]) // this works
-//console.log(jsUser.mySim1) // gives correct output but wrong
-//console.log(typeof mySim1)  // string  // shows why its wrong
+//console.log(jsUser.mySym1) // gives correct output but wrong
+//console.log(typeof mySym1)  // string  // shows why its wrong
 
-console.log(jsUser[mySim1]) //correct way to print
-console.log(typeof mySim1)
-console.log(mySim1);  // Symbol(key1)
+console.log(jsUser[mySym]) //correct way to print
+console.log(typeof mySym)
+console.log(mySym);  // Symbol(key1)
 
 //🟢The first key1 is just the Symbol’s name
 //🟢 The second myKey1 is the data/value stored in the object
 
-// symbol ko object me keys ke roop me add kiya jaata hai, kind of weak encapsulation maana jaata h
+jsUser.email = "Ayush2025@chatgpt.com"
+//Object.freeze(jsUser)
+jsUser.email = "Ayush2026@chatgpt.com"  // don't get updated as it's freezed
+console.log(jsUser)
 
+jsUser.greeting = function(){
+    console.log("Hello js user!")
+}
+jsUser.greetingTwo = function(){
+    console.log(`Hello js user , ${this.name}`)   //Current object jiske andar ye function chal raha hai(this.name  →  jsUser.name  →  "Ayush")
+
+}
+console.log(jsUser.greeting)  //[Function (anonymous)]
+console.log(jsUser.greeting()) 
+console.log(jsUser.greetingTwo()) 
