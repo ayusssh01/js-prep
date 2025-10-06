a522bc15262f82092c78b4daf0a78987fbf454c0
Ayush Jha <121605370+ayusssh01@users.noreply.github.com>
2025-10-06 20:23:55 +0000

    studying objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,3 +20,22 @@ console.log(regularUser.fullname.userfullname.firstname)
 //console.log(regularUser.fullname?.userfullname.firstname) 
 //fullname? --> if fullname exists it'll carry on, otherwise it won't
 
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+//const obj3 ={obj1 , obj2}  // object ke andar objects
+//const obj3  = Object.assign({},obj1,obj2); //Yeh curly braces {} ek empty object bana rahe hain, jo target ke roop me kaam karta hai
+//console.log(obj3)
+
+const obj3 = {...obj1,...obj2}  // most preferred,spread
+console.log(obj3)
+
+const user = [{
+    id : "149",
+    email : "Aryan1708@gmail.com"},
+    {
+    id : "150",
+    email : "Aryan1709@gmail.com"
+    }
+]
+console.log(user[1].email)
